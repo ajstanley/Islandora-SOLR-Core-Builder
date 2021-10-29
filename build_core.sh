6935a3f08ec1360e8f1a5cd0e1556dcd8350b820
Alan Stanley <alanjarlathstanley@gmail.com>
2021-10-29 10:18:37 -0300

    Update build_core.sh

--- build_core.sh
+++ build_core.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Building core SAMPLE on $1";
-docker exec -it $1_solr_1 mkdir -p /opt/solr/server/solr/TESTER1
-docker cp ~/Downloads/conf/  $1_solr_1:/opt/solr/server/solr/TESTER1
-docker exec -it  $1_solr_1 chown -R solr:solr /opt/solr/server/solr/TESTER1
-docker exec -it  $1_solr_1 /opt/solr/bin/solr create_core -c TESTER1 -d /opt/solr/server/solr/TESTER1/conf -force
+echo "Building core ISLANDORA on $1";
+docker exec -it $1_solr_1 mkdir -p /opt/solr/server/solr/ISLANDORA
+docker cp ~/Downloads/conf/  $1_solr_1:/opt/solr/server/solr/ISLANDORA
+docker exec -it  $1_solr_1 chown -R solr:solr /opt/solr/server/solr/ISLANDORA
+docker exec -it  $1_solr_1 /opt/solr/bin/solr create_core -c ISLANDORA -d /opt/solr/server/solr/ISLANDORA/conf -force
