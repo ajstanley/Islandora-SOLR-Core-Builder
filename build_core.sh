07ee883381413307d8e115925f50e1d62535fc9a
Camerin Figueroa <cam@camcodes.com>
2021-11-01 12:30:37 -0400

    Fixed problem with docker cp's path On line 5, the script tries to copy from ~/Downloads/conf This is a non-existing directory by default and seems to be mistake

--- build_core.sh
+++ build_core.sh
@@ -2,6 +2,6 @@
 echo "Building core ISLANDORA on $1";
 docker exec -it $1_solr_1 mkdir -p /opt/solr/server/solr/ISLANDORA
 docker exec -it $1_solr_1 mkdir /opt/solr/server/solr/configsets
-docker cp ~/Downloads/conf/  $1_solr_1:/opt/solr/server/solr/ISLANDORA
+docker cp ./conf/  $1_solr_1:/opt/solr/server/solr/ISLANDORA
 docker exec -it  $1_solr_1 chown -R solr:solr /opt/solr/server/solr/ISLANDORA
 docker exec -it  $1_solr_1 /opt/solr/bin/solr create_core -c ISLANDORA -d /opt/solr/server/solr/ISLANDORA/conf -force
